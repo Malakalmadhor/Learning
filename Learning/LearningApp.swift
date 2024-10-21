c9b64f63899f82e6e4023fc0a73b31930fd2f446
malakalmadhor <malakalmadhor@outlook.com>
2024-10-21 09:13:45 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Malak on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
