a7de38061f229b21629e8c661c4ce5816f263142
malakalmadhor <malakalmadhor@outlook.com>
2024-10-22 09:48:13 +0300

    changes

--- Learning/ContentView.swift
+++ Learning/ContentView.swift
@@ -8,17 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    
+    @State private var name: String = ""
 
-#Preview {
-    ContentView()
+    
+    var body: some View {
+       ZStack {
+            Color.black
+                .ignoresSafeArea()
+        
+           
+            Circle()
+               .fill(Color.gray)
+               .frame(width: 150, height: 150)
+               .opacity(0.2)
+               .padding(.bottom , 450)
+        
+           
+           Text("🔥")
+               .padding(.bottom, 450)
+               .font(.system(size: 70))
+           
+           VStack (alignment: .leading ) {
+               
+               
+               Text("Hello Learner!")
+                   .foregroundColor(.white)
+                   .font(.largeTitle)
+                   .bold()
+               
+               Spacer()
+                   .frame(height: 10)
+               
+               Text("This app will help you learn everday")
+                   .foregroundColor(.white)
+                   .opacity(0.3)
+                   .font(.system(size: 18))
+               
+               Spacer()
+                   .frame(height: 50)
+
+               Text("I want to learn")
+                   .foregroundColor(.white)
+                   .font(.system(size: 18))
+    
+               
+           }
+           .padding(.trailing, 70)
+        
+
+           
+           TextField("Swift", text: $name)
+                      .textFieldStyle(.plain)
+                      .keyboardType(.default)
+                      .font(.system(size: 18))
+                      .padding(.top, 200)
+                      .padding(.leading, 30)
+               
+           Rectangle()
+               .fill(Color.gray)
+               .frame(height: 1)
+               .padding(.top, 250)
+               .opacity(0.3)
+               .padding()
+                           
+           HStack {
+               
+            
+            
+               
+               
+               
+        }
+           }
+           
+        }
+        
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
 }
